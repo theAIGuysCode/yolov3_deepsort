a0a050c76d383d9aa8a834d13bd04b79c05a0bbc
The AI Guy <56439609+theAIGuysCode@users.noreply.github.com>
2020-03-27 17:03:34 -0400

    Initial upload of code to implement object tracking.

---
+++ object_tracker.py
@@ -0,0 +1,162 @@
+import time, random
+import numpy as np
+from absl import app, flags, logging
+from absl.flags import FLAGS
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from yolov3_tf2.models import (
+    YoloV3, YoloV3Tiny
+)
+from yolov3_tf2.dataset import transform_images
+from yolov3_tf2.utils import draw_outputs, convert_boxes
+
+from deep_sort import preprocessing
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+from PIL import Image
+
+flags.DEFINE_string('classes', './data/labels/coco.names', 'path to classes file')
+flags.DEFINE_string('weights', './weights/yolov3.tf',
+                    'path to weights file')
+flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
+flags.DEFINE_integer('size', 416, 'resize images to')
+flags.DEFINE_string('video', './data/video/paris.mp4',
+                    'path to video file or number for webcam)')
+flags.DEFINE_string('output', None, 'path to output video')
+flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
+flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
+
+
+def main(_argv):
+    # Definition of the parameters
+    max_cosine_distance = 0.5
+    nn_budget = None
+    nms_max_overlap = 1.0
+    
+    #initialize deep sort
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
+
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    if len(physical_devices) > 0:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    if FLAGS.tiny:
+        yolo = YoloV3Tiny(classes=FLAGS.num_classes)
+    else:
+        yolo = YoloV3(classes=FLAGS.num_classes)
+
+    yolo.load_weights(FLAGS.weights)
+    logging.info('weights loaded')
+
+    class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
+    logging.info('classes loaded')
+
+    times = []
+
+    try:
+        vid = cv2.VideoCapture(int(FLAGS.video))
+    except:
+        vid = cv2.VideoCapture(FLAGS.video)
+
+    out = None
+
+    if FLAGS.output:
+        # by default VideoCapture returns float instead of int
+        width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(vid.get(cv2.CAP_PROP_FPS))
+        #print("fps: ", fps)
+        codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
+        out = cv2.VideoWriter(FLAGS.output, codec, fps, (width, height))
+        list_file = open('detection.txt', 'w')
+        frame_index = -1 
+    
+    fps = 0.0
+    count = 0 
+    while True:
+        _, img = vid.read()
+
+        if img is None:
+            logging.warning("Empty Frame")
+            time.sleep(0.1)
+            count+=1
+            if count < 3:
+                continue
+            else: 
+                break
+
+        img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+        img_in = tf.expand_dims(img_in, 0)
+        img_in = transform_images(img_in, FLAGS.size)
+
+        t1 = time.time()
+        boxes, scores, classes, nums = yolo.predict(img_in)
+        converted_boxes = convert_boxes(img, boxes[0])
+        #print(converted_boxes)
+        features = encoder(img, converted_boxes)    
+        #score to 1.0 here, change in future to use scores
+        detections = [Detection(bbox, score, cls, feature) for bbox, score, cls, feature in zip(converted_boxes, scores[0], classes[0], features)]
+        
+        #initialize color map
+        cmap = plt.get_cmap('tab20b')
+        colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+
+        # run non-maxima suppresion
+        boxs = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        indices = preprocessing.non_max_suppression(boxs, nms_max_overlap, scores)
+        detections = [detections[i] for i in indices]        
+
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections)
+
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue 
+            bbox = track.to_tlbr()
+            color = colors[int(track.track_id) % len(colors)]
+            color = [i * 255 for i in color]
+            cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            cv2.rectangle(img, (int(bbox[0]), int(bbox[1]-25)), (int(bbox[0]+60), int(bbox[1])), color, -1)
+            cv2.putText(img, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (255,255,255),2)
+            
+        #for det in detections:
+        #    bbox = det.to_tlbr() 
+        #    cv2.rectangle(img,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
+        
+        # print fps on screen 
+        fps  = ( fps + (1./(time.time()-t1)) ) / 2
+        cv2.putText(img, "FPS: {:.2f}".format(fps), (0, 30),
+                          cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+        cv2.imshow('output', img)
+        if FLAGS.output:
+            out.write(img)
+            frame_index = frame_index + 1
+            list_file.write(str(frame_index)+' ')
+            if len(converted_boxes) != 0:
+                for i in range(0,len(converted_boxes)):
+                    list_file.write(str(converted_boxes[i][0]) + ' '+str(converted_boxes[i][1]) + ' '+str(converted_boxes[i][2]) + ' '+str(converted_boxes[i][3]) + ' ')
+            list_file.write('\n')
+
+        # press q to quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+    vid.release()
+    if FLAGS.ouput:
+        out.release()
+        list_file.close()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
