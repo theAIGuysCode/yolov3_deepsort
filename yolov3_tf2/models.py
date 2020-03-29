311ee218503fa5908f1cf241572c7c3356dfb77c
The AI Guy <56439609+theAIGuysCode@users.noreply.github.com>
2020-03-29 16:35:29 -0400

    Updated model to have proper flags

--- yolov3_tf2/models.py
+++ yolov3_tf2/models.py
@@ -22,9 +22,6 @@ from tensorflow.keras.losses import (
 from .batch_norm import BatchNormalization
 from .utils import broadcast_iou
 
-yolo_max_boxes = 100
-yolo_iou_threshold = 0.5
-yolo_score_threshold = 0.5
 # customize your model through the following parameters
 flags.DEFINE_integer('yolo_max_boxes', 100, 'maximum number of detections at one time')
 flags.DEFINE_float('yolo_iou_threshold', 0.5, 'iou threshold')
@@ -195,10 +192,10 @@ def yolo_nms(outputs, anchors, masks, classes):
         boxes=tf.reshape(bbox, (tf.shape(bbox)[0], -1, 1, 4)),
         scores=tf.reshape(
             scores, (tf.shape(scores)[0], -1, tf.shape(scores)[-1])),
-        max_output_size_per_class=yolo_max_boxes,
-        max_total_size=yolo_max_boxes,
-        iou_threshold=yolo_iou_threshold,
-        score_threshold=yolo_score_threshold
+        max_output_size_per_class=FLAGS.yolo_max_boxes,
+        max_total_size=FLAGS.yolo_max_boxes,
+        iou_threshold=FLAGS.yolo_iou_threshold,
+        score_threshold=FLAGS.yolo_score_threshold
     )
 
     return boxes, scores, classes, valid_detections
